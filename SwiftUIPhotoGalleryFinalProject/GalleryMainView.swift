9cda58cf1a579bae22eb3d1e1238c229d8fbb3d8
hutility-norwin <norwin@hutility.com>
2022-12-03 15:09:17 -0800

    Initial commit

---
+++ SwiftUIPhotoGalleryFinalProject/GalleryMainView.swift
@@ -0,0 +1,20 @@
+//
+//  GalleryMainView.swift
+//  SwiftUIPhotoGalleryFinalProject
+//
+//  Created by Norwin Uy on 2022-12-03.
+//
+
+import SwiftUI
+
+struct GalleryMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GalleryMainView_Previews: PreviewProvider {
+    static var previews: some View {
+        GalleryMainView()
+    }
+}
